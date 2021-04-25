9ae2b3ad65b7bfe7398dc56806424682a0c28f6c
Babakailash <nathkailash2020@gmail.com>
2021-04-25 12:23:48 +0530

    Added In Branches

--- Demo3.java
+++ Demo3.java
@@ -2,6 +2,10 @@ public class Demo3
 {
 public static void main(String[] args)
 {
+<<<<<<< HEAD
 System.out.println("Demo2 Program-Confi");
+=======
+System.out.println(" Demo2 ProgramConfilect");
+>>>>>>> eff1af3c06fd0f8cdac7f84721fe634000d552a5
 }
 }
